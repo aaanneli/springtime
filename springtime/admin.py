82cc3656dcf11d518d461d26b59c2844c2fbd6ef
Elise Craig <2191401C@student.gla.ac.uk>
2017-03-04 22:01:07 +0000

    Created User Profile

--- springtime/admin.py
+++ springtime/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from springtime.models import UserProfile
 
 # Register your models here.
+admin.site.register(UserProfile)
--- springtime/models.py
+++ springtime/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+
+	def __str__(self):
+		return self.user.username
+
+	def __unicode__(self):
+		return self.user.username
