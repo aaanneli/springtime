b56885fe646bc2dbd5db4c1a6f11766651606a02
Sunshiine99 <i-l-y_93@hotmail.com>
2017-03-15 18:55:53 +0000

    Models created and admin panel updated

--- springtime/admin.py
+++ springtime/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from springtime.models import UserProfile
+from springtime.models import UserProfile, Trampoline, Booking, Review
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Trampoline)
+admin.site.register(Booking)
+admin.site.register(Review)
--- springtime/models.py
+++ springtime/models.py
@@ -4,11 +4,36 @@ from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
-	user = models.OneToOneField(User)
+    user = models.OneToOneField(User)
 
-	def __str__(self):
+    def __str__(self):
 		return self.user.username
 
-	def __unicode__(self):
+    def __unicode__(self):
 		return self.user.username
 
+class Trampoline(models.Model):
+    trampolineID = models.CharField(max_length=10, primary_key = True)
+    broken = models.BooleanField()
+    category = models.CharField(max_length=20)
+
+class Booking(models.Model):
+    refNumber = models.IntegerField(primary_key = True)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    trampolineID = models.ForeignKey(Trampoline)
+    userID = models.ForeignKey(User)
+
+class Review(models.Model):
+    revNumber = models.IntegerField(primary_key = True)
+    time = models.DateField(auto_now=True)
+    userID = models.ForeignKey(User)
+    content = models.CharField(max_length=120)
+    rating = models.IntegerField()
+
+
+
+
+
+    
+    
