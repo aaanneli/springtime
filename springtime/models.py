fcae78527ffafedc626a415cfe69dc9dc5330726
Sunshiine99 <i-l-y_93@hotmail.com>
2017-03-16 02:51:34 +0000

    Modified ManyToMany relationships

--- springtime/models.py
+++ springtime/models.py
@@ -5,6 +5,8 @@ from django.db import models
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
 		return self.user.username
@@ -13,27 +15,34 @@ class UserProfile(models.Model):
 		return self.user.username
 
 class Trampoline(models.Model):
-    trampolineID = models.CharField(max_length=10, primary_key = True)
+    trampolineID = models.CharField(max_length=10, primary_key = True, unique = True)
     broken = models.BooleanField()
-    category = models.CharField(max_length=20)
+    category = models.CharField(max_length=20, null = False)
+
+    def __str__(self):
+		return self.trampolineID
+
+    def __unicode__(self):
+		return self.trampolineID
 
 class Booking(models.Model):
-    refNumber = models.IntegerField(primary_key = True)
-    startTime = models.DateTimeField()
-    endTime = models.DateTimeField()
-    trampolineID = models.ForeignKey(Trampoline)
-    userID = models.ForeignKey(User)
+    refNumber = models.IntegerField(primary_key = True, unique = True)
+    startTime = models.DateTimeField(null = False)
+    endTime = models.DateTimeField(null = False)
+    trampolineID = models.ManyToManyField(Trampoline)
+    userID = models.ManyToManyField(User)
+
 
 class Review(models.Model):
-    revNumber = models.IntegerField(primary_key = True)
+    revNumber = models.IntegerField(primary_key = True, unique = True)
     time = models.DateField(auto_now=True)
     userID = models.ForeignKey(User)
     content = models.CharField(max_length=120)
-    rating = models.IntegerField()
+    rating = models.IntegerField(null = False)
 
+    def __str__(self):
+		return self.content
 
+    def __unicode__(self):
+		return self.content
 
-
-
-    
-    
