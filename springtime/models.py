30643756705e60585f5eb6eb00617f89586e2ee8
Sunshiine99 <i-l-y_93@hotmail.com>
2017-03-16 18:46:46 +0000

    Added categories to populate script!

--- springtime/models.py
+++ springtime/models.py
@@ -34,7 +34,7 @@ class Category(models.Model):
 class Trampoline(models.Model):
     trampolineID = models.CharField(max_length=8, primary_key = True, unique = True)
     broken = models.BooleanField(default = False)
-    category = models.CharField(max_length=20, null = False)
+    category = models.ForeignKey(Category)
 
     def __str__(self):
         return self.trampolineID
