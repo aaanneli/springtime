d96e9c9f8d4a20128616e7b6ba3614acdbb823c1
springtime <anneli.pedersen@hotmail.com>
2017-03-23 04:02:58 +0000

    added userID to review page. reviews now working again. no longer showing broken trampolines

--- springtime/forms.py
+++ springtime/forms.py
@@ -13,16 +13,17 @@ class ReviewForm(forms.ModelForm):
     content = forms.CharField(max_length=300,
                                 help_text="Tell us about your experience:")
     rating = forms.IntegerField(help_text="Rate your experience:", min_value=1, max_value=5)
+    userID = User.username
 
     class Meta:
         model = Review
-        fields = ('content', 'rating')
-        
-        
+        fields = ('userID', 'content', 'rating')
+
+
 class BookingForm(forms.ModelForm):
     date = forms.DateField(help_text="Chose a date you would like to come for a jump", widget = forms.SelectDateWidget(years=[2017, 2018]))
     details = forms.CharField(max_length=150, help_text="Please inform us of any special requirements you may have:")
-    
+
     class Meta:
         model = Booking
         fields = ('date', 'details')
